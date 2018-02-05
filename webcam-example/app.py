95d24d0d264e8ad1489ad898d7edfc4f00883461
Wolf Zander <mmz216@line24.doc.ic.ac.uk>
2018-02-05 21:00:53 +0000

    Added manage buffer

--- webcam-example/app.py
+++ webcam-example/app.py
@@ -1,5 +1,7 @@
 import flask
 import json
+import os
+import shutil
 
 def create_app():
     app = flask.Flask(__name__)
@@ -22,3 +24,36 @@ def upload_photo():
     file = flask.request.files['webcam']
     file.save("./snaps/" + "snap_{}.jpg".format(counter))
     return flask.make_response(json.dumps({"status": "ok"}))
+
+def get_image_names(directory_):
+    files = os.listdir(directory_)
+    image_present = False
+    images = []
+    for file in files:
+        if file.endswith('.jpg') or file.endswith('.png'):
+            images.append(file)
+            image_present = True
+
+    if not image_present:
+        print("Could not find any Images!")
+    return images
+
+def manage_buffer():
+    while True:
+        snaps = "snaps"
+        dir = os.getcwd()
+
+        image_list = get_image_names(os.path.join(dir, snaps))
+        print(len(image_list))
+
+        if len(image_list) == 8:
+            for i in range(4):
+                file_path = os.path.join(dir, snaps, image_list[i])
+                print(file_path)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                except Exception as e:
+                    print(e)
+
+manage_buffer()
