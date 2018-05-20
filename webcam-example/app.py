6700d86514b1a4875827855e46878bbfb2fd05d5
PeterHedley94 <phedley17@imperial.ac.uk>
2018-05-20 22:24:24 +0100

    Peter Changes

--- webcam-example/app.py
+++ webcam-example/app.py
@@ -32,7 +32,7 @@ array_buffer = collections.deque(maxlen=4)
 input_q = queue.LifoQueue(maxsize=3)
 
 #Load the model
-model_path = "/Users/manuelzander/Computer_Science/Ocado/group-project-front-end/webcam-example/intermediate.hdf5"
+model_path = "checkpoints/intermediate.hdf5"#"/home/peter/Documents/Imperial/Ocado/group-project-back-end/MODEL_OUTPUTS/checkpoints/intermediate.hdf5"#
 model = keras.models.load_model(model_path)
 model._make_predict_function()
 graph = tf.get_default_graph()
@@ -46,7 +46,7 @@ def send_to_server_webcam():
     base64_data = codecs.encode(string, 'base64')
     image_bytes = io.BytesIO(base64.b64decode(base64_data))
     image = np.array(Image.open(image_bytes))
-    array = cv2.resize(image, (250, 250))
+    array = cv2.resize(image, (250,250))#(250, 250))
     array = (array / 255)
 
     #Append array to a circular buffer
@@ -60,22 +60,28 @@ def predict():
             list_of_images = []
             for item in array_buffer:
                 list_of_images.append(item)
-
+            print(len(list_of_images))
             pictures = np.asarray(list_of_images)
+            print(pictures.shape)
 
             with graph.as_default():
-                predictions = model.predict(pictures)
+                predictions = model.predict(pictures[:,:,:,0:3])
+                print(predictions)
+        
 
             #Selected products that we can purchase
-            indices = [1,4,6,9,10,13,14,15,16,17]
+            indices = [0,1,2,3,4,5,6,7,9,10,11,12]
+            #indices = [1,4,6,9,10,13,14,15,16,17]
             predictions = np.sum(predictions[:,indices],axis = 0)
             predictions = predictions/sum(predictions)
             predictions = predictions.tolist()
+            
             input_q.put(predictions)
 
 #Send predictions thread
 def send_thread():
     while True:
+        time.sleep(1)
         if (len(array_buffer) >= 4):
             predictions = input_q.get()
             socketio.emit('scan', json.dumps(predictions))
